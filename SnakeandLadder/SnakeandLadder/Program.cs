a17b895f13b2c33bdbde2b4b30c4fe6c4da6a6fd
unknown <kinekarpallavi21@gmail.com>
2022-03-10 11:45:34 +0530

    Single player at position 0

--- SnakeandLadder/SnakeandLadder/Program.cs
+++ SnakeandLadder/SnakeandLadder/Program.cs
@@ -1,10 +1,13 @@
 ﻿public class SnakesAndLadders
 {
 
-	public static void Main(String[] args)
-	{
-		  Console.WriteLine("Welcome to Snake and Ladder game");
-		
-	}
+	    static void Main(String[] args)
+
+	  {
+		int position = 0; 
+        Console.WriteLine("Welcome to Snake and Ladder game");
+        Console.WriteLine(" Current position: " + position);
+        
+      }
 }
 
