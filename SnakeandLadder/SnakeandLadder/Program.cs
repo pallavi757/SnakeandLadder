de4b78d1a84274d57101b83d8049b2ba7a4340eb
unknown <kinekarpallavi21@gmail.com>
2022-03-10 12:47:05 +0530

    Player check for NoPlay,Ladder or Snake

--- SnakeandLadder/SnakeandLadder/Program.cs
+++ SnakeandLadder/SnakeandLadder/Program.cs
@@ -1,14 +1,35 @@
 ﻿public class SnakesAndLadders
 {
 
-        public static void Main(String[] args)
+     public static void Main(String[] args)
 
 	  {
-		int position = 0;
+        Console.WriteLine("Welcome to Snake and Ladder game");
+
+        int position = 0;
         Random random = new Random();
         int RollDie = random.Next(1, 6);//To get number between 1 to 6
-        Console.WriteLine("Welcome to Snake and Ladder game");
-        Console.WriteLine(" Current position: " + position);
+        Random CheckOption=new Random();
+        Console.WriteLine("The dice was rolled to: " + RollDie);
+        int NoPlay_Ladder_Snake=CheckOption.Next(0,3);
+        switch(NoPlay_Ladder_Snake)
+        {
+            case 0:
+                Console.WriteLine("Player action: nothing");
+            break;
+            case 1:
+                Console.WriteLine("Player action: ladder");
+                position += RollDie;
+                break;
+            case 2:
+                Console.WriteLine("Player action: snake");
+                position-=RollDie;
+                break;
+
+
+        }
+        Console.WriteLine("Current position: " + position);
+       
         
       }
 }
