641d4ae70400afc43441dc8215448bc617c8aec6
unknown <kinekarpallavi21@gmail.com>
2022-03-10 12:12:43 +0530

    The Player rolls the die

--- SnakeandLadder/SnakeandLadder/Program.cs
+++ SnakeandLadder/SnakeandLadder/Program.cs
@@ -4,7 +4,9 @@
         public static void Main(String[] args)
 
 	  {
-		int position = 0; 
+		int position = 0;
+        Random random = new Random();
+        int RollDie = random.Next(1, 6);//To get number between 1 to 6
         Console.WriteLine("Welcome to Snake and Ladder game");
         Console.WriteLine(" Current position: " + position);
         
